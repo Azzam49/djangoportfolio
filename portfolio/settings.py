faff422d549a0b292c843aa36e5b3394771ec661
Azzam <48876472+Azzam49@users.noreply.github.com>
2020-11-02 23:08:49 +0800

    Update settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -17,7 +17,6 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
-import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -173,4 +172,3 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Configure Django App for Heroku.
 
-django_heroku.settings(locals())
